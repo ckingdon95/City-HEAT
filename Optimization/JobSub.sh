65e403cbc95b40f9b7f6dced30d58199cd3ff3f1
Cora Kingdon <ckingdon@berkeley.edu>
2023-05-24 13:39:55 -0700

    update sbatch arguments for savio

--- Optimization/JobSub.sh
+++ Optimization/JobSub.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=MOEA_ma
-#SBATCH --nodes=3            					 		        # Total number of nodes to request (up to 120)
-#SBATCH --ntasks-per-node=24		               				        # Number of processors per node (up to 20)
-#SBATCH --partition=parallel          				 			# Queue name "parallel"
+#SBATCH --nodes=1            					 		        # Total number of nodes to request (up to 120)
+#SBATCH --cpus-per-task=24		               				        # Number of processors per node (up to 20)
+#SBATCH --partition=savio2          				 			# Queue name "parallel"
 #SBATCH --time=10:00:00      					 			# Run time (hh:mm:ss) - up to 36 hours
-#SBATCH --mail-user=rshi8@jhu.edu   		                			# address for email notification
-#SBATCH --mail-type=ALL                  	                		        # email at Begin and End of job
+#SBATCH --account=fc_anthofflab
 
 #module load gcc openmpi 
 
