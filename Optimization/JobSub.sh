2536fa6b071cd5b53473abd6c902a190d679eab6
Cora Kingdon <ckingdon@berkeley.edu>
2023-05-24 13:48:32 -0700

    update sbatch args

--- Optimization/JobSub.sh
+++ Optimization/JobSub.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 #SBATCH --job-name=MOEA_ma
-#SBATCH --nodes=1            					 		        # Total number of nodes to request (up to 120)
-#SBATCH --cpus-per-task=24		               				        # Number of processors per node (up to 20)
-#SBATCH --partition=savio2          				 			# Queue name "parallel"
-#SBATCH --time=10:00:00      					 			# Run time (hh:mm:ss) - up to 36 hours
 #SBATCH --account=fc_anthofflab
+#SBATCH --partition=savio          				 			
+#SBATCH --nodes=1            					 		        
+#SBATCH --cpus-per-task=24		               				        
+#SBATCH --time=1:00:00      					 			
 
 #module load gcc openmpi 
 
