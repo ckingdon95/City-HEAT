c479b2d3e73dfa250413745054e2f9d8bb56c62f
Cora Kingdon <ckingdon@berkeley.edu>
2024-02-28 11:54:27 -0800

    add back array args

--- Optimization/find_runtime_metrics.sh
+++ Optimization/find_runtime_metrics.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=FindMetrics_ma
 #SBATCH --account=fc_anthofflab
+#SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
+#SBATCH -p parallel				# Queue name "standard" (serial)
+#SBATCH --array=1-3				# Array of jobs to loop through
 #SBATCH --partition=savio          				 			
 #SBATCH --nodes=1            					 		        
 #SBATCH --time=1:00:00      
